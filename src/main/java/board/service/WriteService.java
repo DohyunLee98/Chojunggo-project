cf8cd30c62e6156da075a6a47d7c7a8284abda19
smdude <qazcdef23@naver.com>
2024-06-07 15:51:39 +0900

    0607_1551

--- src/main/java/board/service/WriteService.java
+++ src/main/java/board/service/WriteService.java
@@ -13,29 +13,45 @@ import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 public class WriteService {
 
-	public List<String> uploadImages(HttpServletRequest request, HttpServletResponse response, Writer writer) throws Exception {
+	public List<String> uploadImages(HttpServletRequest request, HttpServletResponse response, Writer writer)
+			throws Exception {
 		String uploadPath = "C:/Users/SJ02/git/group-project/src/main/webapp/image/uploadedImages";
 		List<String> imageList = new ArrayList<>();
+
+		checkDirectory(uploadPath);  
 		
-		File uploadDir = new File(uploadPath);
-		if (!uploadDir.exists()) {
-			uploadDir.mkdirs();
-		}
-		
-		RenameFile renamedFile = new RenameFile(writer);
-		MultipartRequest multi = new MultipartRequest(request, uploadPath, 5 * 1024 * 1024, "UTF-8", new DefaultFileRenamePolicy());
-		
+		MultipartRequest multi = new MultipartRequest(request, uploadPath, 5 * 1024 * 1024, "UTF-8",
+				new DefaultFileRenamePolicy());
+
 		Enumeration files = multi.getFileNames();
-//		while(files.hasMoreElements()) {
-//			String file = (String) files.nextElement();
-//			String fileName = multi.getFilesystemName(file);
-//			String realFileName = multi.getOriginalFileName(fileName);
-//			System.out.println(fileName);
-//			imageList.add(realFileName);
-//		}
 		
+		while (files.hasMoreElements()) {
+			String file = (String) files.nextElement();
+			String fileName = multi.getFilesystemName(file);
+			String fileRealName = multi.getOriginalFileName(file);
+
+			if (fileName != null) {
+				File originFile = new File(uploadPath + "/" + fileName);
+				String originFileName = originFile.getName();
+				String ext = originFileName.substring(originFileName.lastIndexOf("."));
+				String renamedFile = writer.getNickname() + ext;
+				File tempFile = new File(uploadPath + "/" + renamedFile);
+
+				if (!originFile.renameTo(tempFile)) {
+					System.err.println("파일명 변경 실패");
+				}
+				imageList.add(renamedFile);
+				System.out.println(renamedFile);
+			}
+
+		}
+
 		return imageList;
 	}
-	
-	
+
+	public void checkDirectory(String uploadPath) {
+		File uploadDir = new File(uploadPath); 
+		if (!uploadDir.exists())
+			uploadDir.mkdirs();  // 해당 폴더가 없을 경우 생성
+	}
 }
