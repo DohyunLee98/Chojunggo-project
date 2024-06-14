356826cdbb3bc1b9410f4c27410877d9a51948ba
LEE DO HYUN <leedh9806@naver.com>
2024-06-14 15:44:12 +0900

    1234

---
+++ src/main/java/board/model/PayRequest.java
@@ -0,0 +1,13 @@
+package board.model;
+
+public class PayRequest {
+
+	private String title;
+	private int price;
+
+	public PayRequest(String title, int price) {
+		this.title = title;
+		this.price = price;
+	}
+
+}
